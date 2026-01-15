ae3c0768009aa1f75bede72b0dff55403caeb033
fares2.718 <faresobaid2715@gmail.com>
2026-01-15 16:17:55 +0400

    Added response interseptor

--- PlacePickerClient/src/main.ts
+++ PlacePickerClient/src/main.ts
@@ -2,15 +2,23 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import {
+  HttpEventType,
   HttpHandlerFn,
   HttpRequest,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function logginIterseptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
-  console.log('[OutGoinng Request]', request);
-  return next(request);
+  console.log('[Outgoing Request]', request);
+  return next(request).pipe(
+    tap((event) => {
+      if (event.type === HttpEventType.Response) {
+        console.log('[Incomming Request]', event.status, event.body);
+      }
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, {
